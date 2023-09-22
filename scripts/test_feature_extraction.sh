2aaa0e9b7277ffa32df2b4b76d8129592f3f0a8d
emrecaslan <aslane84@students.rowan.edu>
2023-09-22 11:25:38 -0400

    stable

--- scripts/test_feature_extraction.sh
+++ scripts/test_feature_extraction.sh
@@ -3,11 +3,11 @@
 #test
 
 python3 test_extract_feature_maps.py \
---data data/rw_small.yaml \
+--data data/large.yaml \
 --device 5 \
 --img 480 \
---batch 20 \
+--batch 1 \
 --conf 0.5 \
 --iou 0.5 \
---weights "/home/aslane84/curriculum_yolov7/runs/train/CL_yolov7_112/weights/epoch_049.pt" \
+--weights "/home/aslane84/curriculum_yolov7/weights/epoch_049.pt" \
 --name   "CL_yolov7_"
